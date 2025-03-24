6bbdf2e3fbc73e23ede0a5397039f7981b4860fa
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-03-24 11:07:39 +0100

    completed task 1 (missing advanced complex number representation)

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -20,11 +20,26 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class ComplexNumber(re: Double, im: Double)
+    type Complex = ComplexNumber
+
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
+
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexNumber(re, _) => re
+
+      def im(): Double = complex match
+        case ComplexNumber(_, im) => im
+
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re1, im1), ComplexNumber(re2, im2)) => ComplexNumber(re1 + re2, im1 + im2)
+
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re1, im1), ComplexNumber(re2, im2)) => ComplexNumber(re1 - re2, im1 - im2)
+
+      def asString(): String = s"${complex.re()} + ${complex.im()}i"
+      /*def asString(): String = complex match
+        case ComplexNumber(re, im) if im == 0.0 => s"${if re < 0.0 then "-" else ""}$re"
+        case ComplexNumber(re, im) if re == 0.0 => s"${if im < 0.0 then "-" else ""}${im}i"
+        case ComplexNumber(re, im) => s"${if re < 0.0 then "-" else ""}$re ${if im < 0.0 then "- " else "+ "}${im}i"*/
